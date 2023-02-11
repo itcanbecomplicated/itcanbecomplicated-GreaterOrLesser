14ec742c00f1ce89e0f60616c6d54576829ffa84
Nikita <itcanbecomplicatedinfo@gmail.com>
2023-02-11 22:02:04 +0000

    code challenge3

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertThrows;
 
 public class Lab {
     /**
@@ -10,6 +11,12 @@ public class Lab {
      *          If neither is true, return "Equal". This problem is case-sensitive!
      */
     public String decide(int a, int b){
-        return "Equal";
+        if (a<b) {
+            return "Lesser";
+        } else if (a>b)  {
+            return "Greater";
+        } else {
+            return "Equal";
+        }
     }
 }
